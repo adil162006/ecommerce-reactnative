4f96ce7e251625dfdb49906963e17e2abaa431f3
Mohd Adil <shaikhmdadil191@gmail.com>
2025-12-25 12:57:20 +0530

    deployment configurations done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import path from "path"
 import {connectDB} from './config/db.js';
 import { ENV } from "./config/env.js";
 
 const app = express();
 
+const __dirname = path.resolve();
 
 
 app.get('/', (req, res) => {
@@ -13,6 +15,16 @@ app.get('/', (req, res) => {
 app.get("/api/health", (req, res) => {
   res.status(200).send("OK");
 });
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
+
 
 const startServer = async () => {
   await connectDB();
