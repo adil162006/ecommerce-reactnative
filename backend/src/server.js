c30139de411601e594d5b32e1c68b3929c15a178
Mohd Adil <shaikhmdadil191@gmail.com>
2025-12-28 12:18:35 +0530

    feature:added order controller and  order route

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,6 +10,7 @@ import { ENV } from "./config/env.js";
 
 import adminRoutes from "./routes/admin.route.js";
 import userRoutes from "./routes/user.route.js";
+import orderRoutes from "./routes/order.route.js";
 const app = express();
 
 const __dirname = path.resolve();
@@ -20,6 +21,7 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 app.get("/api/health", (req, res) => {
   res.status(200).send("OK");
 });
