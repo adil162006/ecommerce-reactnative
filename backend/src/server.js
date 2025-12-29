56dd3d316420b7c9be684d84c7f90af9aec52f31
Mohd Adil <shaikhmdadil191@gmail.com>
2025-12-29 19:44:47 +0530

    added cors configuration

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,6 +20,8 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.use("/api/admin", adminRoutes);
